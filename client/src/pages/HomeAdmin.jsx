ff1b4c45e6cfcd16b2f8deb45b074ca88e8484cd
mafiasoz247 <mafiasoz123@gmail.com>
2022-04-21 18:28:58 +0500

    final home admin completed

--- client/src/pages/HomeAdmin.jsx
+++ client/src/pages/HomeAdmin.jsx
@@ -11,16 +11,21 @@ const HomeAdmin = () => {
       <body>  
         {/* <div class="container-homeadmin"><img class="cover" src="./img/undraw.png" /></div> */}
         <div class="zone blue grid-wrapper">
-          <div class="box zone">
+          
+          <div class="boxfirst zone">
           <img src="https://cdn2.iconfinder.com/data/icons/complete-common-version-6-4/1024/add_library-512.png"></img>
           <h1>Create Library</h1>
+          </div> 
+          <div class="box zone">
+          <img src="https://cdn0.iconfinder.com/data/icons/users-android-l-lollipop-icon-pack/24/group2-256.png"></img>
+          <h1>Users</h1>
           </div>
-          <div class="box zone"></div>
+          <div class="boxlast zone">
+          <img src="https://cdn1.iconfinder.com/data/icons/education-39/100/15-128.png"></img>
+          <h1>Libraries</h1>
+          </div>
           
-          <div class="box zone"></div>
           
-          <div class="box zone"></div>
-          <div class="box zone"></div>
           
         </div>
         
