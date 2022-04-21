af6aa1ee4fcda8118572225c21e06df93b77c53b
mafiasoz247 <mafiasoz123@gmail.com>
2022-04-21 17:00:12 +0500

    homeadminicons

--- client/src/Components/Navbar/MenuItems.js
+++ client/src/Components/Navbar/MenuItems.js
@@ -15,6 +15,7 @@ export const MenuItems = [
         cName: 'nav-links'
     },
     
+    
 ]
 export const MenuItemsAdmin = [
     {
--- client/src/pages/HomeAdmin.jsx
+++ client/src/pages/HomeAdmin.jsx
@@ -11,7 +11,10 @@ const HomeAdmin = () => {
       <body>  
         {/* <div class="container-homeadmin"><img class="cover" src="./img/undraw.png" /></div> */}
         <div class="zone blue grid-wrapper">
-          <div class="box zone"></div>
+          <div class="box zone">
+          <img src="https://cdn2.iconfinder.com/data/icons/complete-common-version-6-4/1024/add_library-512.png"></img>
+          <h1>Create Library</h1>
+          </div>
           <div class="box zone"></div>
           
           <div class="box zone"></div>
